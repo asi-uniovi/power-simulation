a748b1a66688b4881556c159fd59721ce3849eee
Ramón Medrano Llamas <m3drano@gmail.com>
2014-10-02 07:55:59 +0200

    Add increment() to Stats.
    Former-commit-id: 76871050231f4f0caa82abc021b5e6e786ed849f

--- stats.py
+++ stats.py
@@ -5,3 +5,7 @@ from singleton import Singleton
 
 class Stats(dict, metaclass=Singleton):
     """This is just a singleton dict with some helpers."""
+
+    def increment(self, key, inc=1):
+        """Increments by inc a key. Creates the key if not existing."""
+        self[key] = self.get(key, 0) + inc
