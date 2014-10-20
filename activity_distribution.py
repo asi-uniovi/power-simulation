cda640d229d0ad53c340fb82feb944689cc29fac
Ramón Medrano Llamas <m3drano@gmail.com>
2014-10-20 09:11:32 +0200

    Add distribution handling and timestamp conversion.
    Former-commit-id: 16e76231dd6e103757676b06982b1239a047e812

--- activity_distribution.py
+++ activity_distribution.py
@@ -1,6 +1,7 @@
 """User activiy distribution parsing and managing."""
 
 import csv
+import numpy
 from collections import defaultdict
 
 DAYS = {
@@ -12,6 +13,15 @@ DAYS = {
     'Friday': 5,
     'Saturday': 6,
 }
+
+DISTRIBUTIONS = {
+    'exp': numpy.random.exponential,
+}
+
+
+HOUR = lambda x: x * 60 * 60
+DAY = lambda x: x * HOUR(24)
+WEEK = lambda x: x * DAY(7)
 
 
 def float_es(string):
@@ -26,9 +36,9 @@ class ActivityDistribution(object):
     intervals that start on each hour.
     """
 
-    def __init__(self, filename, distribution=None):
+    def __init__(self, filename, distribution):
         self._histogram = defaultdict(lambda: defaultdict(float))
-        self._distribution = distribution
+        self._distribution = DISTRIBUTIONS[distribution]
         self.__load_trace(filename)
 
     def avg_inactivity_for_hour(self, day, hour):
@@ -40,6 +50,17 @@ class ActivityDistribution(object):
         if self._distribution:
             return self._distribution(self.avg_inactivity_for_hour(hour, day))
         raise RuntimeError('Distribution is not defined for this model')
+
+    def random_inactivity_for_timestamp(self, timestamp):
+        """Queries the activity distribution and generates a random sample."""
+        day, hour = self._timestamp_to_day(timestamp)
+        return self.random_inactivity_for_hour(day, hour)
+
+    def _timestamp_to_day(self, timestamp):
+        day, hours = divmod(timestamp % WEEK(1), DAY(1))
+        assert 0 <= day <= 6, 'Invalid day index yielded'
+        assert 0 <= hours // HOUR(1) <= 23, 'Invalid hour index yielded'
+        return day, hours // HOUR(1)
 
     def __load_trace(self, filename):
         """Parses the CSV with the trace formatted {day, hour, inactivity}."""
