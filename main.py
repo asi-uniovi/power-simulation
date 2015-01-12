d2ad94258d88b97f891196b40ed4cfd6da2baeab
Ramón Medrano Llamas <m3drano@gmail.com>
2015-01-12 07:49:43 +0100

    Better logging in main.py
    Former-commit-id: c51932fcb5991d3ccff49b0da32f858fffa35a38

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 """Main runner of the simulation."""
 
 from __future__ import print_function
+
 import argparse
 import logging
 import os
@@ -10,16 +11,20 @@ import sys
 import ConfigParser as configparser
 from simulation import Simulation
 
-# TODO: configure logging level via cmd line flags.
-logging.basicConfig(format='%(asctime)s %(levelname)s(%(name)s): %(message)s',
-                    datefmt='%d/%m/%Y %H:%M:%S',
-                    level=logging.DEBUG)
-logger = logging.getLogger(__name__)
+
+def config_logging(debug):
+    """Sets logging basic config"""
+    logging.basicConfig(
+        format='%(asctime)s %(levelname)s(%(name)s): %(message)s',
+        datefmt='%d/%m/%Y %H:%M:%S',
+        level=logging.DEBUG if debug else logging.INFO)
 
 
 def parse_arguments():
     """Get the arguments in a standard way."""
     parser = argparse.ArgumentParser()
+    parser.add_argument("-d", "--debug", help="show debug information",
+                        action="store_true")
     parser.add_argument('--config', dest='config_file', default='config.ini')
     return parser.parse_args()
 
@@ -38,9 +43,10 @@ def main():
     """Just starts the simulation."""
     try:
         args = parse_arguments()
+        config_logging(args.debug)
         config = parse_config(args.config_file)
         Simulation(config).run()
-        logging.info('Program finished')
+        logging.info('Simulation finished')
     except:
         logging.exception('Unexpected exception')
         return 1
