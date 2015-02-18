7d02e8686fdc586b93c604cf976acf2d4925b820
Ramón Medrano Llamas <m3drano@gmail.com>
2015-02-18 22:49:31 +0100

    Very quick hack to read "Trace" files and fit a pareto.
    Former-commit-id: 44af2693ac27388f914a06bf734d40e1ca1506e9

--- activity_distribution.py
+++ activity_distribution.py
@@ -1,8 +1,11 @@
 """User activity distribution parsing and managing."""
 
 import csv
+import functools
 import numpy
 import logging
+import scipy
+import scipy.stats
 from base import Base
 from collections import defaultdict
 from singleton import Singleton
@@ -50,7 +53,7 @@ class ActivityDistribution(Base, metaclass=Singleton):
         self._histogram = defaultdict(lambda: defaultdict(float))
         self._distribution = DISTRIBUTIONS[distribution]
         self._env = env
-        self.__load_trace(filename)
+        self.__load_raw_trace_and_fit(filename)
 
     def avg_inactivity_for_hour(self, day, hour):
         """Queries the activity distribution to the get average inactivity."""
@@ -58,8 +61,11 @@ class ActivityDistribution(Base, metaclass=Singleton):
 
     def random_inactivity_for_hour(self, day, hour):
         """Queries the activity distribution and generates a random sample."""
+        inactivity = self.avg_inactivity_for_hour(day, hour)
+        if callable(inactivity):
+            return inactivity()
         if self._distribution:
-            return self._distribution(self.avg_inactivity_for_hour(day, hour))
+            return self._distribution(inactivity)
         raise RuntimeError('Distribution is not defined for this model')
 
     def random_inactivity_for_timestamp(self, timestamp):
@@ -93,6 +99,30 @@ class ActivityDistribution(Base, metaclass=Singleton):
                 logger.info('Distr.: avg(%.2f), std(%.2f), [%.2f; %.2f]',
                             numpy.average(data), numpy.std(data),
                             numpy.min(data), numpy.max(data))
+            except csv.Error as error:
+                raise RuntimeError(('Error reading {}:{}: {}'
+                                    .format(filename, trace.line_num, error)))
+
+    def __load_raw_trace_and_fit(self, filename):
+        """Parses the CSV with the trace formatted {day, hour, inactivity*}."""
+        with open(filename) as trace:
+            try:
+                dialect = csv.Sniffer().sniff(trace.read(1024))
+                trace.seek(0)
+                reader = csv.reader(trace, dialect)
+
+                next(reader)
+                for item in reader:
+                    day = item[0]
+                    hour = item[1]
+                    shape, scale, loc = scipy.stats.pareto.fit(
+                        numpy.asarray(list(map(float, item[2:]))))
+                    self._histogram[DAYS[day]][int(hour)] = (
+                        functools.partial(lambda s, l: (numpy.random.pareto(s) + 1) * l,
+                                          shape, loc))
+
+                    logger.info(
+                        'Distribution fitted to pareto of shape = %f loc = %f', shape, loc)
             except csv.Error as error:
                 raise RuntimeError(('Error reading {}:{}: {}'
                                     .format(filename, trace.line_num, error)))
