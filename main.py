7687aad207279bb191f00ae7b59d3100dbf3a830
Ramón Medrano Llamas <m3drano@gmail.com>
2015-02-24 21:42:03 +0100

    Major compatibility changes for Python 2.x
    Former-commit-id: 00107236ffe9f8cb9ab5f4bc4dccccc84cec483c

--- main.py
+++ main.py
@@ -2,8 +2,10 @@
 
 """Main runner of the simulation."""
 
+from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
+from __future__ import unicode_literals
 
 import argparse
 import logging
