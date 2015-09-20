e73f5b8bb8de2d6281fe7c2c6b1a4ce607cd09b6
Ramón Medrano Llamas <m3drano@gmail.com>
2015-09-20 18:03:59 +0200

    Add mean() and median() to the distributions.
    Former-commit-id: d6897da874101d74fb9989ef5960f29b4cf78a89

--- distribution.py
+++ distribution.py
@@ -13,6 +13,16 @@ logger = logging.getLogger(__name__)  # pylint: disable=invalid-name
 class Distribution(six.with_metaclass(abc.ABCMeta)):
     """Base distribution class."""
 
+    @property
+    def mean(self):
+        """Expected value for the distribution."""
+        raise NotImplementedError
+
+    @property
+    def median(self):
+        """Expected 50th percentile for the distribution."""
+        raise NotImplementedError
+
     def rvs(self):
         """This samples the distribution for one value."""
         raise NotImplementedError
@@ -44,6 +54,16 @@ class EmpiricalDistribution(Distribution):
         # pylint: disable=no-member
         ecdf = sm.distributions.ECDF(numpy.array(data, copy=True))
         self._inverse = sm.distributions.monotone_fn_inverter(ecdf, ecdf.x)
+        self._mean = numpy.mean(data)
+        self._median = numpy.median(data)
+
+    @property
+    def mean(self):
+        return self._mean
+
+    @property
+    def median(self):
+        return self._median
 
     def rvs(self):
         return float(self._inverse(numpy.random.random()))
@@ -56,6 +76,15 @@ class BinomialDistribution(Distribution):
         # pylint: disable=invalid-name
         self._N = N
         self._p = p
+
+    @property
+    def mean(self):
+        return self._N * self._p
+
+    @property
+    def median(self):
+        # https://dx.doi.org/10.1111%2Fj.1467-9574.1980.tb00681.x
+        return round(self._N * self._p)
 
     def rvs(self):
         return self.xrvs(1)
