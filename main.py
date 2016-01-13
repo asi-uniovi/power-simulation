249aa92a8652d98c6502f62c774171a78a315a42
Ramón Medrano Llamas <m3drano@gmail.com>
2016-01-13 21:23:44 +0100

    Log warnings with logging.
    Former-commit-id: 7405df115f6586b73d7552c3eb412df07078cb3e

--- main.py
+++ main.py
@@ -33,6 +33,7 @@ def config_logging(debug):
         format='%(asctime)s %(levelname)s(%(name)s): %(message)s',
         datefmt='%d/%m/%Y %H:%M:%S',
         level=logging.DEBUG if debug else logging.INFO)
+    logging.captureWarnings(True)
 
 
 def parse_arguments():
