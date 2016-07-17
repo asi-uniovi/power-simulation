883b0beb217604234d0c6c9123298ec0402f049a
Ramón Medrano Llamas <m3drano@gmail.com>
2016-07-17 20:12:47 +0200

    numpy takes half of memory than tuple().
    Former-commit-id: f9dbde54d4a42beddfdace0ea76aa5a66765cabf

--- distribution.py
+++ distribution.py
@@ -9,7 +9,8 @@ class Distribution(object, metaclass=abc.ABCMeta):
     """Base distribution class."""
 
     def __init__(self, data):
-        self.__data = tuple(data)
+        self.__data = numpy.sort(data)
+        self.__data.setflags(write=False)
 
     @property
     def data(self):
@@ -49,9 +50,9 @@ class EmpiricalDistribution(Distribution):
 
     def __init__(self, data):
         super(EmpiricalDistribution, self).__init__(sorted(data))
-        self.__diffs = tuple(
-            [self.data[i + 1] - self.data[i]
-             for i in range(self.sample_size - 1)] + [0.0])
+        self.__diffs = numpy.asarray([self.data[i + 1] - self.data[i]
+            for i in range(self.sample_size - 1)] + [0.0])
+        self.__diffs.setflags(write=False)
 
     def rvs(self):
         """Implementation from "Simulation Modeling and Analysis, 5e"."""
