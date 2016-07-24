0636e759b68adf062018b70a88c776b7cf502a2d
Ramón Medrano Llamas <m3drano@gmail.com>
2016-07-24 22:35:25 +0200

    Fix missing seal() method.
    Former-commit-id: bb767b3ffd56efd8b79d85c3f8ecfde59e99ef5a

--- hashable.py
+++ hashable.py
@@ -5,19 +5,35 @@ import numpy
 
 # pylint: disable=too-few-public-methods
 class HashableArray(object):
-    """This is just contains the NumPy array and the hash."""
+    """This just contains the NumPy array and the hash."""
 
     def __init__(self, data, sort=False):
         super(HashableArray, self).__init__()
+        self.__sealed = False
         try:
             self.__hash = hash(data)
         except TypeError:
             self.__hash = hash(tuple(data))
         if sort:
-            self.__array = numpy.sort(data)
+            self.__array = sorted(data)
         else:
-            self.__array = numpy.asarray(data)
-        self.__array.setflags(write=False)
+            self.__array = list(data)
+
+    def seal(self, sort=False):
+        """Makes the array a numpy array and makes it immutable."""
+        if not self.__sealed:
+            if sort:
+                self.__array = numpy.sort(self.__array)
+            else:
+                self.__array = numpy.asarray(self.__array)
+            self.__array.setflags(write=False)
+            self.__sealed = True
+
+    def extend(self, other):
+        """Extends the array contents with new array."""
+        if self.__sealed:
+            raise RuntimeError('Array is sealed, cannot extend.')
+        self.__array.extend(other)
 
     @property
     def array(self):
