422bcaa72aaf331a7e376f06a5077d8b6f570fce
Ramón Medrano Llamas <m3drano@gmail.com>
2017-02-05 19:40:19 +0100

    Add some limits for the histogram plotter and misc. fixes.

--- tools/plot_histogram.py
+++ tools/plot_histogram.py
@@ -13,10 +13,12 @@ import scipy.stats
 from tools.parse_trace import parse_trace
 
 
-def plot_histogram(trace, key, nbins, distribution_to_fit):
+def plot_histogram(trace, key, nbins, distribution_name, xmax):
     """Plots a trace."""
     all_items = numpy.asarray([
         i for pc in trace.values() for i in pc[key]])
+
+    distribution = getattr(scipy.stats, distribution_name)
 
     if nbins is None:
         # Use the Freedman-Diaconis estimate.
@@ -26,18 +28,19 @@ def plot_histogram(trace, key, nbins, distribution_to_fit):
         logging.warning('Using %d bins as default for %d samples.',
                         nbins, len(all_items))
 
-    shape, loc, scale = getattr(scipy.stats, distribution_to_fit).fit(all_items)
-    fit = getattr(scipy.stats, distribution_to_fit)(shape, loc, scale)
+    fit = distribution(*distribution.fit(all_items))
 
     matplotlib.pyplot.style.use('bmh')
     _, axis = matplotlib.pyplot.subplots(1, 1)
 
-    data, bins, _ = axis.hist(all_items, nbins, normed=1,
+    data, bins, _ = axis.hist(all_items, nbins, normed=True,
                               label='Histogram for key "%s"' % key)
     axis.plot(bins, fit.pdf(bins), 'r--', linewidth=1,
-              label='Best %s fit for key "%s"' % (distribution_to_fit, key))
+              label='Best %s fit for key "%s"' % (distribution_name, key))
 
     axis.set_ylim([0.0, max(data)])
+    if xmax is not None:
+        axis.set_xlim([0.0, xmax])
     matplotlib.pyplot.xlabel('Interval duration (s)')
     matplotlib.pyplot.ylabel('Probability')
     axis.grid(True, which='both')
@@ -60,10 +63,13 @@ def main():
     parser.add_argument('--distribution_to_fit',
                         dest='distribution_to_fit', default='norm',
                         help='which distribution to fit')
+    parser.add_argument('--xmax',
+                        dest='xmax', type=int, default=None,
+                        help='max for the x axis on the plots')
     args = parser.parse_args()
     try:
         plot_histogram(parse_trace(args.trace_file), args.key, args.nbins,
-                       args.distribution_to_fit)
+                       args.distribution_to_fit, args.xmax)
     except KeyError:
         print('Invalid key: %s' % args.key)
         return 1
