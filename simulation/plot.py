38c66ab368815fab937bc947304e972bdfba5cde
Ramón Medrano Llamas <m3drano@gmail.com>
2017-05-23 18:58:28 +0200

    Plot the K-T for the histograms.

--- simulation/plot.py
+++ simulation/plot.py
@@ -21,6 +21,7 @@ import injector
 import matplotlib.cm as cm
 import matplotlib.pyplot as plt
 import numpy
+import scipy.stats
 from simulation.activity_distribution import DistributionFactory
 from simulation.base import Base
 from simulation.static import DAYS
@@ -46,6 +47,7 @@ class Plot(Base):
             self.plot_hourly_histogram_count(histogram)
             self.plot_mean_medians_comparison(histogram)
             self.plot_hourly_histogram_quantiles(histogram)
+            self.plot_ks_test(histogram)
         except KeyError:
             logger.warning('Histogram %s produced no plot.', histogram)
 
@@ -120,10 +122,45 @@ class Plot(Base):
         fig.savefig('%s_count.png' % histogram.lower())
         plt.close(fig)
 
+    def plot_ks_test(self, histogram: str) -> None:
+        """Generates a plot for the K-S test for a given histogram."""
+        fig, ax1 = plt.subplots()
+        ax1.set_title('%s (K-S)' % histogram)
+        ax1.set_xlim(0, 7 * 24 - 1)
+        ax2 = ax1.twinx()
+
+        hists = self.__stats.get_all_hourly_histograms(histogram)
+        distributions = (
+            self.__training_distribution.get_all_hourly_distributions(
+                histogram))
+        ks, p = [], []
+        for h, d in zip(hists, distributions):
+            if h.size == 0 or not d:
+                ks.append(0.0)
+                p.append(0.0)
+                continue
+            D, pvalue = scipy.stats.kstest(h, d.cdf)
+            ks.append(D)
+            p.append(pvalue)
+
+        # pylint: disable=no-member
+        x = numpy.linspace(1, len(ks), len(ks))
+        ax1.plot(x, ks, label='K-S D statistic')
+        ax2.plot(x, p, label='p-value', color='red')
+
+        _format_ax_line(ax1, legend=False)
+        _format_ax_line(ax2, legend=False)
+        fig.set_size_inches(6, 5)
+        fig.set_tight_layout(True)
+        fig.savefig('%s_ks.png' % histogram.lower())
+        plt.close(fig)
+
+
 # pylint: disable=invalid-name
-def _format_ax_line(ax) -> None:
+def _format_ax_line(ax, legend: bool = True) -> None:
     """Common format for each of the axes."""
-    ax.legend(loc='upper center', fontsize=8)
+    if legend:
+        ax.legend(loc='upper center', fontsize=8)
     ax.grid(True)
     ax.set_xticks(numpy.arange(7) * 24)
     ax.set_xticklabels(
