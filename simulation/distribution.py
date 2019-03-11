5d3f7a6940dd2bc332fd75a476da363e17f4b20f
Ramón Medrano Llamas <m3drano@gmail.com>
2019-03-11 22:29:48 +0100

    More modern spline usage.

--- simulation/distribution.py
+++ simulation/distribution.py
@@ -29,7 +29,7 @@ class EmpiricalDistribution:
 
     def __init__(self, data: typing.Any):
         self.__data = numpy.asarray(data)
-        self.__tck = None
+        self.__spline = None
 
     @property
     def data(self) -> numpy.ndarray:
@@ -50,21 +50,21 @@ class EmpiricalDistribution:
         """Sample the spline that has the inverse CDF."""
         if self.__data.size < 2:
             return numpy.random.choice(self.__data, size=size)
-        if self.__tck is None:
-            self.__fit_tck()
-        return scipy.interpolate.splev(
-            numpy.random.random(size=size), self.__tck, der=0)
+        if self.__spline is None:
+            self.__fit_spline()
+        return self.__spline(numpy.random.random(size=size), nu=0)
 
     def extend(self, other: 'EmpiricalDistribution') -> None:
         """This extends this distribution with data from another."""
         self.__data = numpy.append(self.__data, other.data)
-        self.__tck = None
+        self.__spline = None
 
-    def __fit_tck(self) -> None:
+    def __fit_spline(self) -> None:
         """Fits the distribution for generating random values."""
         self.__data.sort()
-        self.__tck = scipy.interpolate.splrep(
+        t, c, k = scipy.interpolate.splrep(
             numpy.linspace(0, 1, self.__data.size), self.__data, k=1)
+        self.__spline = scipy.interpolate.BSpline(t, c, k, extrapolate=False)
 
     def __len__(self) -> int:
         return len(self.__data)
